85e1db4b3913d0e5b6adfaab268335967c675d29
Paula Ho <pho2@chicagobooth.edu>
2023-04-11 14:10:50 +0000

    Defined Application for Square Root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,15 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
 
-  def blank_square_root_form
+  def square_root_form
     render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def square_root_results
+    #params = {"user_number"}
+    @num = params.fetch("user_number").to_f
+    @square_root_of_num = @num ** 0.5
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
   end
 
 end
